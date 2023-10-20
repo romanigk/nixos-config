352cbf7e1f4a6270e1b9acfec48cfa8848cfe04c
Robert Manigk <romanigk@mailbox.org>
2023-10-20 20:16:19 +0200

    Add basic configuration of my laptop

--- flake.nix
+++ flake.nix
@@ -1,11 +1,45 @@
 {
-  description = "A very basic flake";
+  description = "Personal Nix and NixOS Configuration";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+	config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        pulse-gen1 = lib.nixosSystem {
+	  inherit system;
+	  modules = [
+	    ./configuration.nix
+	    home-manager.nixosModules.home-manager {
+	      home-manager.useGlobalPkgs = true;
+	      home-manager.useUserPackages = true;
+	      home-manager.users.p1ng0ut = {
+	        imports = [ ./home.nix ];
+	      };
+	    }
+	  ];
+	};
+      };
+      homeManagerConfiguration = {
+        pulse-gen1 = home-manager.lib.homeManagerConfiguration {
+	  inherit system pkgs;
+	  modules = [
+	    ./home.nix
+	  ];
+	};
+      };
+    };
 }
