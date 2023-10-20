ca951eeb9fe2e7592a5f855aadda37296420e782
Robert Manigk <romanigk@mailbox.org>
2023-10-20 23:35:59 +0200

    Add thunderbird, neovim, git and wget

--- configuration.nix
+++ configuration.nix
@@ -88,7 +88,7 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       firefox
-    #  thunderbird
+      thunderbird
     ];
   };
 
@@ -98,8 +98,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    git
+    neovim
+    wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
