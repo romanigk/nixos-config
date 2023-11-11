ce9fdf395ad0abe536266265eac2a6521dafb8dd
Robert Manigk <p1ng0ut@mailbox.org>
2023-11-11 17:33:56 +0100

    Add fish shell to nixos configuration too

--- configuration.nix
+++ configuration.nix
@@ -100,6 +100,8 @@
     tmux
   ];
 
+  programs.fish.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
