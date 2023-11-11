32a89db2fa0eb977b4eae42f26dee8198b91bedc
Robert Manigk <p1ng0ut@mailbox.org>
2023-11-11 18:07:47 +0100

    Set defaultUserShell to fish

--- configuration.nix
+++ configuration.nix
@@ -81,7 +81,8 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.defaultUserShell = pkgs.fish;
+
   users.users.p1ng0ut = {
     isNormalUser = true;
     description = "Robert Manigk";
