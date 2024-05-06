387b9dce4eae0a68a7dff75bdd5313afdff9105c
Robert Manigk <p1ng0ut@mailbox.org>
2024-05-06 19:27:01 +0200

    Remove less useful comments

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -41,9 +41,7 @@
       #   });
       # })
     ];
-    # Configure your nixpkgs instance
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
     };
   };
