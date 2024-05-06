dcd69adeb6660994f037215ee37fbb4fc98343d7
Robert Manigk <p1ng0ut@mailbox.org>
2024-05-06 23:22:55 +0200

    switch back to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -2,15 +2,11 @@
   description = "p1ng0uts new nix config, based on 'github:misterio77/nix-starter-config#standard'";
 
   inputs = {
-    # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    # You can access packages and modules from different nixpkgs revs
-    # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # 1Password
