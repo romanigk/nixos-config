be2218114c378d0e712474f84ebe2a5163765e5e
Robert Manigk <p1ng0ut@mailbox.org>
2024-05-09 10:18:20 +0200

    Use external systems definition

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    systems.url = "github:nix-systems/default-linux";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -12,17 +14,27 @@
     self,
     nixpkgs,
     home-manager,
+    systems,
     ...
   } @ inputs: let
     inherit (self) outputs;
-    system = "x86_64-linux";
+    lib = nixpkgs.lib // home-manager.lib;
+    forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs (import systems) (
+      system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        }
+    );
   in {
-    packages = import ./pkgs nixpkgs.${system};
-    formatter.${system} = packages.alejandra;
-
-    overlays = import ./overlays {inherit inputs;};
+    inherit lib;
     nixosModules = import ./modules/nixos;
     homeManagerModules = import ./modules/home-manager;
+
+    overlays = import ./overlays {inherit inputs;};
+
+    formatter = forEachSystem (pkgs: pkgs.alejandra);
 
     nixosConfigurations = {
       pulse15-gen1 = nixpkgs.lib.nixosSystem {
