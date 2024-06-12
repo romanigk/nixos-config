5fbd5a5c7f6522a598ce304abb08f2dd40157d8b
Robert Manigk <p1ng0ut@mailbox.org>
2024-06-12 19:23:21 +0200

    Remove unused rycee firefox addons

--- flake.nix
+++ flake.nix
@@ -10,11 +10,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
