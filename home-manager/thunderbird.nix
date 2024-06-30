5562e9ceeeef18b1704dc7b87a4f2166dd58dd35
Robert Manigk <p1ng0ut@mailbox.org>
2024-06-30 17:56:09 +0200

    nix fmt

--- home-manager/thunderbird.nix
+++ home-manager/thunderbird.nix
@@ -1,12 +1,12 @@
 {
-    config,
-    pkgs,
-    ...
+  config,
+  pkgs,
+  ...
 }: {
   programs.thunderbird = {
     enable = true;
     profiles.p1ng0ut = {
-        isDefault = true;
+      isDefault = true;
     };
   };
 }
