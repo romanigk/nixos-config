1a6cf8b6c1f17b703a29ece74f94f76459fe02d4
Robert Manigk <p1ng0ut@mailbox.org>
2024-11-04 19:46:45 +0100

    add public nixos-hardware repository

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
     systems.url = "github:nix-systems/default-linux";
 
+    hardware.url = "github:nixos/nixos-hardware";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
