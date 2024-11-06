84eb4c1e5ecfa8bc90c56506db7485952fb57788
Robert Manigk <p1ng0ut@mailbox.org>
2024-11-06 15:04:51 +0100

    reorder services in configuration nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -77,23 +77,38 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  services.xserver = {
-    enable = true;
+  services = {
+    fwupd.enable = true;
 
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
-    xkb = {
-      layout = "de";
-      variant = "";
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+
+    printing.enable = true;
+
+    xserver = {
+      enable = true;
+
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+
+      xkb = {
+        layout = "de";
+        variant = "";
+      };
     };
   };
 
   console.keyMap = "de";
-
-  services.fwupd.enable = true;
-
-  services.printing.enable = true;
 
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -123,19 +138,6 @@
       viAlias = true;
       vimAlias = true;
     };
-  };
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   hardware = {
