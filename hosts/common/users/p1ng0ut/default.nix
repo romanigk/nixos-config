874d40229ed2c81c7d09cf97eaf9d520bb98c4d2
Robert Manigk <p1ng0ut@mailbox.org>
2025-04-29 22:37:56 +0200

    Format files w/ nix fmt

--- hosts/common/users/p1ng0ut/default.nix
+++ hosts/common/users/p1ng0ut/default.nix
@@ -1,23 +1,28 @@
-{ pkgs, config, lib, ...}: let
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
   ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
-  in {
-    users.mutableUsers = false;
-    users.users.p1ng0ut = {
-      isNormalUser = true;
-      shell = pkgs.fish;
-      extraGroups = ifTheyExist [
-        "audio"
-	"docker"
-	"git"
-	"networkmanager"
-	"podman"
-	"video"
-	"wheel"
-      ];
+in {
+  users.mutableUsers = false;
+  users.users.p1ng0ut = {
+    isNormalUser = true;
+    shell = pkgs.fish;
+    extraGroups = ifTheyExist [
+      "audio"
+      "docker"
+      "git"
+      "networkmanager"
+      "podman"
+      "video"
+      "wheel"
+    ];
 
     openssh.authorizedKeys.keys = lib.splitString "\n" (builtins.readFile ../../../../home/p1ng0ut/ssh.pub);
     packages = [pkgs.home-manager];
-    };
+  };
 
   home-manager.users.p1ng0ut = import ../../../../home/p1ng0ut/${config.networking.hostName}.nix;
-  }
+}
