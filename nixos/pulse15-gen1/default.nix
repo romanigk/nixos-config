c5d3d32f31e615295b54b01b9e779b9aa0efaf1d
Robert Manigk <105636975+romanigk@users.noreply.github.com>
2025-07-05 13:27:27 +0200

    configure gnome as desktop for thinkpad25 (#8)
    * Add hyprland to configuration of ThinkPad25
    
    * Try to use Gnome instead of hyprland
    
    * Remove configuration.nix and make experimentalFeatures unique
    
    * Remove hardware-configuration.nix
    
    * Clean-Up comments
    
    ---------
    
    Co-authored-by: Robert Manigk <p1ng0ut@mailbox.org>

--- nixos/pulse15-gen1/default.nix
+++ nixos/pulse15-gen1/default.nix
@@ -11,15 +11,7 @@
     ./hardware-configuration.nix
   ];
 
-  # Host-specific configuration for pulse15-gen1
   networking.hostName = "pulse15-gen1";
 
-  # Add any pulse15-gen1 specific configuration here
-  # For example:
-  # - Display configuration for this specific laptop
-  # - Battery optimization settings
-  # - Hardware-specific services
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
