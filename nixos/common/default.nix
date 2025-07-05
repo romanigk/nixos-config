9d5dc86030d440edb3044cc8462c10e96abe945f
Robert Manigk <p1ng0ut@mailbox.org>
2025-07-05 13:23:20 +0200

    Clean-Up comments

--- nixos/common/default.nix
+++ nixos/common/default.nix
@@ -7,7 +7,6 @@
   ...
 }: {
   imports = [
-    # Import common modules
     outputs.nixosModules.default or {}
   ];
 
@@ -27,27 +26,17 @@
   in {
     settings = {
       experimental-features = "nix-command flakes";
-      # Opinionated: disable global registry
       flake-registry = "";
-      # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
     };
-    # Opinionated: disable channels
     channel.enable = false;
 
-    # Opinionated: make registry and nix path match flake inputs
     registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
   };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   networking.networkmanager.enable = true;
 
@@ -75,17 +64,11 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
     };
 
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
- };
+  };
 
   console.keyMap = "de";
 
