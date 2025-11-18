4294b0dcd13674ed5281eddd7d26c5528b208276
Robert Manigk <p1ng0ut@mailbox.org>
2025-11-18 20:18:06 +0100

    Remove unused unstable-packages overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -18,10 +18,4 @@
       config.allowUnfree = true;
     };
   };
-  unstable-packages = final: _prev: {
-    unstable = import inputs.nixpkgs-unstable {
-      system = final.system;
-      config.allowUnfree = true;
-    };
-  };
 }
