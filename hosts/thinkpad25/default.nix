c28cfbad8282aff9c4a5b483fe0943bc502ff4c3
Robert Manigk <p1ng0ut@mailbox.org>
2025-11-18 20:36:11 +0100

    Remove unnecessary configurations from `thinkpad25` system configuration

--- hosts/thinkpad25/default.nix
+++ hosts/thinkpad25/default.nix
@@ -9,32 +9,16 @@
     ./hardware-configuration.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
   networking.hostName = "thinkpad25";
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Europe/Berlin";
 
   services = {
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
   };
 
-  users.users.p1ng0ut = {
-    createHome = true;
-    isNormalUser = true;
-    extraGroups = ["networkmanager" "wheel"];
-  };
-
   environment.systemPackages = with pkgs; [
-    git
-    neovim
     gnome-tweaks
   ];
-
-  nixpkgs.config.allowUnfree = true;
 
   system.stateVersion = "25.05";
 }
