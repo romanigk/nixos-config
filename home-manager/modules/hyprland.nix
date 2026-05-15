a6cbc23ac35e20a1d7b519b37d78a7958e3bb5bc
Robert Manigk <p1ng0ut@mailbox.org>
2026-05-15 07:14:26 +0200

    Handle deprication warning, use configType lua

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -7,6 +7,7 @@
   ...
 }: {
   wayland.windowManager.hyprland = {
+    configType = "lua";
     enable = true;
     package = inputs.hyprland.packages."${pkgs.stdenv.hostPlatform.system}".hyprland;
     xwayland.enable = true;
